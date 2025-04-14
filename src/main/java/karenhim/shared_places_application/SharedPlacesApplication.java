bed6779163bc749b8eba37b06bd67c12d185da5b
Karen <karen.xien@gmail.com>
2025-04-14 12:16:15 +0200

    First commit

---
+++ src/main/java/karenhim/shared_places_application/SharedPlacesApplication.java
@@ -0,0 +1,13 @@
+package karenhim.shared_places_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedPlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedPlacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/karenhim/shared_places_application/SharedPlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package karenhim.shared_places_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedPlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
